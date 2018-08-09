aeb0ae36fcbce567d259aa1f40a80cef9b46fb47
weste <elizabeth.west@cdk.com>
2018-08-09 11:22:27 -0700

    window on a sphere

---
+++ Assets/hackathon.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class hackathon : MonoBehaviour {
+    public float speed = 5f;
+    public Vector3 vector3;
+
+	//private void OnMouseDown()
+	//{
+ //       transform.Translate(0, speed * Time.deltaTime, 0);  
+	//}
+
+	// Use this for initialization
+	void Start () {
+  
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //if(Input.GetButton("UP")){
+        //    transform.Translate(0, speed * Time.deltaTime, 0);
+        //}
+        //if (Input.GetButton("DOWN"))
+        //{
+        //    transform.Translate(0, -speed * Time.deltaTime, 0);
+        //}
+
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            transform.Translate(-Vector3.down * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.down * Time.deltaTime);
+        }
+	}
+
+	
+}
