aa285b7a739b9fb8976037d41fb2941cf1840f97
Darsh Lin <darsh.lin@cdk.com>
2018-08-09 19:14:17 -0700

    Took out some non working code and added textures

---
+++ Assets/Sound.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+public class Sound : MonoBehaviour {
+
+
+    public string Name;
+
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+
+    [HideInInspector]
+    public AudioSource source;
+}
